c595ae0e21dc921dad346686e05f791611248ccd
Gavin Cox <gavin@extramilecommunications.com>
2017-08-25 10:41:51 +0100

    Initial commit

---
+++ assets/js/template.js
@@ -0,0 +1,86 @@
+jQuery(document).ready(function() {
+
+	/*******************************************
+	 * HELPER FUNCTIONS *
+	 *******************************************/
+
+	/*
+	 * Image lazy loading
+	 * Further reading: https://www.appelsiini.net/projects/lazyload
+	 * Example image tag:  <img class="lazy" data-original="img/example.jpg" width="640" height="480">
+	 * Uncomment out the code below and in the index.php file to enable lazyloading
+	 */
+
+	// jQuery("img.lazy").lazyload({
+	//     threshold : 200
+	// });
+
+	/*
+	 * Check alt tags aren't empty for images
+	 * For development use only
+	 */
+
+	// jQuery('img').each(function() {
+	// 	console.log(jQuery(this).attr('alt'));
+	// });
+
+	/*******************************************
+	 * RESPONSIVE NAV *
+	 *******************************************/
+
+	jQuery('.nav-btn').on('click', function() {
+		jQuery('html').addClass('js-nav');
+	});
+
+	jQuery('.close-btn, .js-nav #innerwrap').on('click', function() {
+		jQuery('html').removeClass('js-nav');
+	});
+
+	if (jQuery('.moduletable.services-carousel').length) {
+        jQuery('.moduletable.services-carousel .latest-news-articles').owlCarousel({
+            loop: false,
+            margin: 10,
+            nav: true,
+            dots: true,
+            items: 1
+        });
+    }
+
+    if (jQuery('.moduletable.products-carousel').length) {
+		jQuery('.moduletable.products-carousel .latest-news-articles').owlCarousel({
+	        loop: false,
+            margin: 20,
+            nav: false,
+            dots: true,
+            responsiveClass:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                520:{
+                    items:2
+                },
+                920:{
+                    items:3
+                    //,margin: 28
+                },
+                1360:{
+                    items: 4
+                    ,nav: true
+                    ,margin: 78
+                }
+            }
+		});
+    }
+
+    if (jQuery('.moduletable.news-carousel').length) {
+        jQuery('.moduletable.news-carousel .latest-news-articles').owlCarousel({
+            loop: false,
+            margin: 10,
+            nav: true,
+            dots: true,
+            items: 1
+        });
+    }
+
+});
---
+++ default.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_banners
+ *
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+JLoader::register('BannerHelper', JPATH_ROOT . '/components/com_banners/helpers/banner.php');
+$baseurl = JUri::base();
+?>
+
+
+<?php foreach ($list as $item) : ?>
+    <?php $imageurl = $item->params->get('imageurl'); ?>
+    <?php $alt = $item->params->get('alt'); ?>
+    <?php $alt = $alt ?: $item->name; ?>
+
+    <?php // Just display the image if no link specified ?>
+        <img src="<?php echo $baseurl . $imageurl;?>" alt="<?php echo $alt;?>"/>
+     <?php echo $item->description ?>
+
+<?php endforeach; ?>
